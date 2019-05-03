a0cf9671a97411c6df1d727991a10abdc27a0c31
anamritraj <anandamritraj904@gmail.com>
2019-05-03 10:05:54 +0530

    Add socket.io in frontend.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import socketIOClient from "socket.io-client";
+
 import MainMenu from "../navigation/MainMenu";
 import Content from "../content/Content";
+
+var socket;
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "http://localhost:9000/"
+    };
+    socket = socketIOClient(this.state.endpoint);
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:9000/login?email=anandamritraj904@gmail.com`)
+      .then(res => {
+        if (!res.data.success) {
+          window.location.href = "http://localhost:9000/login";
+        }
+      });
+  }
   render() {
     return (
       <Router>
@@ -16,3 +39,4 @@ class App extends Component {
   }
 }
 export default App;
+export { socket };
--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import ContentHeader from "./content-header";
 import ContentBody from "./content-body";
-
+import { socket } from "../app/App";
 import "./content.css";
 
 export default class Content extends Component {
+  componentDidMount() {
+    socket.emit("join", "Anand's Spotify");
+    socket.on("updated_song", this.updateSong);
+  }
+
+  updateSong = data => {
+    console.log(data);
+  };
+
   render() {
     return (
       <div className="content-wrapper">
