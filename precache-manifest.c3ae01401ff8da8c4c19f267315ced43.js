b21125ce44a7bcf119259745a7493b55bb5c49f8
anamritraj <anandamritraj904@gmail.com>
2019-05-05 16:31:20 +0530

    Updates

---
+++ precache-manifest.c3ae01401ff8da8c4c19f267315ced43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad784d655153dd17981a5a11b8e9dad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7540e4cf8784dfa264",
+    "url": "/static/css/2.768e1b80.chunk.css"
+  },
+  {
+    "revision": "b94a0af8df504ed05104",
+    "url": "/static/css/main.b35e83b7.chunk.css"
+  },
+  {
+    "revision": "5f7540e4cf8784dfa264",
+    "url": "/static/js/2.7c2eb675.chunk.js"
+  },
+  {
+    "revision": "b94a0af8df504ed05104",
+    "url": "/static/js/main.cb697cc0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
